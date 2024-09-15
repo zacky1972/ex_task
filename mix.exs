5fb12123ba2454cde5a0615e78c076879519ad52
Susumu Yamazaki <zacky1972@gmail.com>
2024-09-15 09:47:47 +0900

    build: support Elixir 1.15+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExTask.MixProject do
     [
       app: :ex_task,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
