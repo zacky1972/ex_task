de985af6092c8fa948e081d5c3498d4683cee5ad
Susumu Yamazaki <zacky1972@gmail.com>
2024-09-15 10:37:14 +0900

    remove hello

--- lib/ex_task.ex
+++ lib/ex_task.ex
@@ -2,17 +2,4 @@ defmodule ExTask do
   @moduledoc """
   ExTask: Task, a task runner / build tool, for Mix
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExTask.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
